64e4518185630197713bc62195db5ec54e589235
bipproduction <bip.production.js@gmail.com>
2023-10-15 21:18:14 +0800

    title: auto des: auto note:auto

--- server/index.js
+++ server/index.js
@@ -7,19 +7,19 @@ async function main() {
     child.stdout.on('data', async (data) => {
         d += data
         await new Promise(r => setTimeout(r, 10000))
-        fetch('https://io.wibudev.com/io', {
-            method: "POST",
-            body: JSON.stringify({
-                id: "monit",
-                path: "/htop",
-                data: html(d)
-            }),
-            headers: {
-                "Content-type": "application/json"
-            }
-        }).then(v => v.text()).then((v) => {
-            d = ""
-        })
+        // fetch('https://io.wibudev.com/io', {
+        //     method: "POST",
+        //     body: JSON.stringify({
+        //         id: "monit",
+        //         path: "/htop",
+        //         data: html(d)
+        //     }),
+        //     headers: {
+        //         "Content-type": "application/json"
+        //     }
+        // }).then(v => v.text()).then((v) => {
+        //     d = ""
+        // })
     })
 }
 
