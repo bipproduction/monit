1c1b3c4fc215151e3f51e5ae3a3ffd708aaf40a7
bipproduction <bip.production.js@gmail.com>
2023-10-16 09:31:13 +0800

    title: auto des: auto note:auto

--- server/index.js
+++ server/index.js
@@ -2,21 +2,21 @@ const { exec } = require("child_process")
 const html = require('ansi-html')
 const { fetch } = require('cross-fetch')
 async function main() {
-    const child = exec('htop')
-    child.stdout.on('data', async (data) => {
-        fetch('https://io.wibudev.com/io', {
-            method: "POST",
-            body: JSON.stringify({
-                id: "monit",
-                path: "/htop",
-                data: html(data)
-            }),
-            headers: {
-                "Content-type": "application/json"
-            }
-        })
+    // const child = exec('htop')
+    // child.stdout.on('data', async (data) => {
+    //     fetch('https://io.wibudev.com/io', {
+    //         method: "POST",
+    //         body: JSON.stringify({
+    //             id: "monit",
+    //             path: "/htop",
+    //             data: html(data)
+    //         }),
+    //         headers: {
+    //             "Content-type": "application/json"
+    //         }
+    //     })
 
-    })
+    // })
 }
 
 main()
